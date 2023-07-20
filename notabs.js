3d097780dae5cf3adbd9d5b2ec251de2948020eb
David Oleander <dta@alumni.cmu.edu>
2023-07-19 22:34:25 -0400

    Use popup styling

--- notabs.js
+++ notabs.js
@@ -1,7 +1,9 @@
-chrome.tabs.onCreated.addListener(function (tab) {
-  chrome.windows.get(tab.windowId, {populate: true}, function(window) {
-    if (window.tabs.length > 1) {
-      chrome.windows.create({tabId: tab.id});
-    }
+(function() {
+  chrome.tabs.onCreated.addListener(function (tab) {
+    chrome.windows.get(tab.windowId, {}, function(window) {
+      if (window.type == "normal") {
+        chrome.windows.create({tabId: tab.id, type: "popup"});
+      }
+    });
   });
-});
+})();
