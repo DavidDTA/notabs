5df2b377cb2c4fbf8bd190470aa2aac66379934c
David Oleander <dta@alumni.cmu.edu>
2023-07-19 23:12:11 -0400

    Remove callback delay

--- notabs.js
+++ notabs.js
@@ -1,9 +1,7 @@
 (function() {
   chrome.tabs.onCreated.addListener(function (tab) {
-    chrome.windows.get(tab.windowId, {}, function(window) {
-      if (window.type == "normal") {
-        chrome.windows.create({tabId: tab.id, type: "popup"});
-      }
-    });
+    if (tab.index != 0) {
+      chrome.windows.create({tabId: tab.id, focused: tab.active});
+    }
   });
 })();
